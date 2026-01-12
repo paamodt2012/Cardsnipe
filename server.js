b66c18cb93582825f637bf2c06a14d3c38a07120
paamodt2012 <paamodt2012@gmail.com>
2026-01-12 14:16:04 -0700

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-// Serve index.html for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`CardSnipe server running on port ${PORT}`);
-});
