f0837371f1dc5d71fbf2355617f53271fa29fce4
paamodt2012 <paamodt2012@gmail.com>
2026-01-12 12:50:37 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// Serve index.html for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`CardSnipe server running on port ${PORT}`);
+});
